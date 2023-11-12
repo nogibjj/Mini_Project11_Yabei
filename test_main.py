0ac1585643869b527d76e7990c930443ec188148
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-11-11 21:05:49 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,7 +10,7 @@ dbfs_path = "dbfs:/FileStore/mini_project11"
 api_endpoint = f"https://{databricks_host}/api/2.0"
 
 # Function to validate the existence of a path in DBFS
-def validate_dbfs_path(target_path, http_headers): 
+def test_validate_dbfs_path(target_path, http_headers): 
     try:
         dbfs_response = requests.get(f"{api_endpoint}/dbfs/get-status?path={target_path}", headers=http_headers)
         dbfs_response.raise_for_status()
@@ -20,10 +20,10 @@ def validate_dbfs_path(target_path, http_headers):
         return False
 
 # Function to test the functionality of Databricks configuration
-def run_databricks_test():
+def test_run_databricks():
     auth_headers = {'Authorization': f'Bearer {bearer_token}'}
-    path_exists = validate_dbfs_path(dbfs_path, auth_headers)
+    path_exists = test_validate_dbfs_path(dbfs_path, auth_headers)
     assert path_exists, "DBFS path does not exist or cannot be accessed"
 
 if __name__ == "__main__":
-    run_databricks_test()
+    test_run_databricks()
