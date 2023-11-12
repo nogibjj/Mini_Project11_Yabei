ac5fe5ca317c800503816bbd2500e4f4b514f6bb
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-11-11 21:36:08 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,9 +10,10 @@ FILESTORE_PATH = "dbfs:/FileStore/tables/mini11"
 api_endpoint = f"https://{databricks_host}/api/2.0"
 
 # Function to validate the existence of a path in DBFS
-def test_validate_dbfs_path(path, http_headers): 
+def test_validate_dbfs_path(): 
+    http_headers = {'Authorization': f'Bearer {bearer_token}'}
     try:
-        dbfs_response = requests.get(f"{api_endpoint}/dbfs/get-status?path={path}", headers=http_headers)
+        dbfs_response = requests.get(f"{api_endpoint}/dbfs/get-status?path={FILESTORE_PATH}", headers=http_headers)
         dbfs_response.raise_for_status()
         return 'path' in dbfs_response.json()
     except Exception as error:
@@ -21,9 +22,7 @@ def test_validate_dbfs_path(path, http_headers):
 
 # Function to test the functionality of Databricks configuration
 def test_run_databricks():
-    auth_headers = {'Authorization': f'Bearer {bearer_token}'}
-    path_exists = test_validate_dbfs_path(FILESTORE_PATH, auth_headers)
-    assert path_exists, "DBFS path does not exist or cannot be accessed"
+    assert test_validate_dbfs_path(), "DBFS path does not exist or cannot be accessed"
 
 if __name__ == "__main__":
     test_run_databricks()
